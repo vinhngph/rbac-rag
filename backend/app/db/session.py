f4ddb6d9b3d5090dcf150e45f1473c238c07d381
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-03-22 09:51:03 +0000

    chore: add app/db/session.py

---
+++ backend/app/db/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+from app.core.config import settings
+
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI, echo=False)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
