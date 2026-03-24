1a8281691851540d230e60f1e4a18516f18e033f
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-03-24 10:42:23 +0000

    build: config alembic

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -7,6 +7,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from sqlmodel import SQLModel
+from app.core.config import settings
+from app.orm_models import *
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,7 +24,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
+config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URI)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
