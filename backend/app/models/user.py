0acb1ab9b808de3f379a246b3ed56c095a94350a
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-03-30 06:32:51 +0000

    fix (be): cannot request empty string

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,6 +5,7 @@ from uuid import UUID, uuid4
 from typing import TYPE_CHECKING
 
 from app.models.links import UserDepartmentRoleLink
+from app.core.types import NonEmptyString
 
 if TYPE_CHECKING:
     from app.models.department import Department
@@ -12,7 +13,7 @@ if TYPE_CHECKING:
 
 class UserBase(SQLModel):
     email: EmailStr = Field(unique=True, index=True, sa_type=AutoString)
-    name: str = Field(default="Unknown")
+    name: NonEmptyString
 
     avatar_url: Optional[HttpUrl] = Field(
         default=None, description="Avatar url.", sa_type=AutoString
@@ -29,12 +30,12 @@ class User(UserBase, table=True):
 
 
 class UserRegister(UserBase):
-    plain_text_password: str
+    plain_text_password: NonEmptyString
 
 
 class UserLogin(SQLModel):
     email: EmailStr
-    plain_text_password: str
+    plain_text_password: NonEmptyString
 
 
 class UserRead(UserBase):
