7c04a8f8f48d58401a23e90c7c2902498cb16a56
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-04-02 17:31:26 +0700

    feat (be): stream knowledge status - sse standard

--- backend/app/routers/knowledge.py
+++ backend/app/routers/knowledge.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter, status, BackgroundTasks, UploadFile, File, HTTPException
+from fastapi import (
+    APIRouter,
+    status,
+    BackgroundTasks,
+    UploadFile,
+    File,
+    HTTPException,
+    Header,
+)
+from fastapi.sse import EventSourceResponse, ServerSentEvent
+from collections.abc import AsyncIterable
 from uuid import UUID
 from typing import Annotated
+from json import dumps as json_dumps
+from asyncio import sleep as async_sleep
 
-from app.models.knowledge import KnowledgeRead
+from app.models.knowledge import KnowledgeRead, Knowledge
 from app.db.session import DB_Session
 from app.dependencies import CurrentUser, Allow
 from app.services.zero_trust import zero_trust
 from app.services.worker.knowledge import process_knowledge
+from app.core.constants import KnowledgeStatus
 
 router = APIRouter(
     prefix="/departments/{department_id}/knowledge", tags=["Knowledge Base"]
@@ -40,3 +53,38 @@ async def upload_knowledge(
     background_tasks.add_task(process_knowledge, knowledge_id=knowledge.id)
 
     return knowledge
+
+
+@router.get("/{knowledge_id}/status", response_class=EventSourceResponse)
+async def stream_knowledge_status(
+    department_id: UUID,
+    knowledge_id: UUID,
+    db: DB_Session,
+    last_event_id: Annotated[str | None, Header()] = None,
+) -> AsyncIterable[ServerSentEvent]:
+    last_sent_status = last_event_id
+
+    knowledge = await db.get(Knowledge, knowledge_id)
+    if not knowledge:
+        yield ServerSentEvent(
+            event="error", data=json_dumps({"error": "File not found."}), id="error"
+        )
+        return
+    while True:
+        current_status = knowledge.status.value
+
+        if current_status != last_sent_status:
+            yield ServerSentEvent(
+                event="status_update",
+                data=json_dumps({"status": current_status}),
+                id=current_status,
+                retry=3000,
+            )
+            last_sent_status = current_status
+
+        if knowledge.status in [KnowledgeStatus.COMPLETED, KnowledgeStatus.FAILED]:
+            break
+
+        await async_sleep(2)
+
+        await db.refresh(knowledge)
