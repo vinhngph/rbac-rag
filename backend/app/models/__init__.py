1b676a5e6c4ef20bd62389fabff58a04b37d4e5a
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-04-04 16:50:07 +0700

    fix (be): No module named 'app.models.department'

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,6 @@
 from app.models.permission import Permission
 from app.models.role import Role
-from app.models.department import Department
 from app.models.user import User
 from app.models.knowledge import Knowledge
 
-__all__ = ["Permission", "Role", "Department", "User", "Knowledge"]
+__all__ = ["Permission", "Role", "User", "Knowledge"]
