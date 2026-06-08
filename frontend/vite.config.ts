99e234d46f5c0d9a3f81b3408365c28d75b7323d
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-06-08 03:51:29 +0000

    chore(fe): config development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 8000,
+    host: true,
+  },
+  preview: {
+    port: 8001,
+  },
 });
