0facbf4dd2d1785c7224c2e93540be7fd49a1e0f
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-06-23 08:01:34 +0000

    chore(fe): remove useless

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 8001,
-    host: true,
-  },
-  preview: {
-    port: 8001,
-  },
 });
