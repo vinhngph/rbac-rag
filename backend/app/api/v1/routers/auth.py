3ad2120de3cdf2c6f4ffcfd69c978f4e5801bd9f
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-07-26 08:30:45 +0000

    fix(be/auth): compatible with new int DEBUG env

--- backend/app/api/v1/routers/auth.py
+++ backend/app/api/v1/routers/auth.py
@@ -18,7 +18,7 @@ async def login(response: Response, login_data: UserLogin, db: DB_Session):
         key=settings.JWT_AT_KEY,
         value=access_token,
         httponly=True,
-        secure=False if settings.DEBUG == "true" else True,
+        secure=settings.DEBUG != 1,
         samesite="lax",
         max_age=int(settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES * 60),
     )
@@ -36,7 +36,7 @@ async def register(response: Response, user_in: UserRegister, db: DB_Session):
         key=settings.JWT_AT_KEY,
         value=access_token,
         httponly=True,
-        secure=False if settings.DEBUG == "true" else True,
+        secure=settings.DEBUG != 1,
         samesite="lax",
         max_age=int(settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES * 60),
     )
