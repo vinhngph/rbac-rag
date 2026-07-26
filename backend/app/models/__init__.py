2592b0de6f105dd1278b7ef371920d8cbcade904
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-07-26 08:47:03 +0000

    chore(be/models): alphabetize models in __all__ list

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,4 +5,4 @@ from app.models.permission import Permission
 from app.models.role import Role
 from app.models.user import User
 
-__all__ = ["Permission", "Role", "User", "Knowledge", "ChatSession", "ChatMessage"]
+__all__ = ["ChatMessage", "ChatSession", "Knowledge", "Permission", "Role", "User"]
